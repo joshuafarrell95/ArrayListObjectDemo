c8f4bf15245fc96e228bed6ef4bb7ed05fb4bb31
Joshua Farrell <99381454+joshuafarrell95@users.noreply.github.com>
2022-10-10 14:39:56 +0800

    Add serializable interface

--- src/arraylistobjectdemo/ArrayListObjectDemo.java
+++ src/arraylistobjectdemo/ArrayListObjectDemo.java
@@ -2,12 +2,15 @@ package arraylistobjectdemo;
 
 /* module java.base */
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.Collections;
 import java.util.InputMismatchException;
+import java.util.Vector;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -24,18 +27,18 @@ public class ArrayListObjectDemo {
     public static void main(String[] args) {
         //ArrayList<Car> cars = new ArrayList<>();
         String fileName;
-        
+
         if (checkFile(args)) {
             fileName = args[1];
         } else {
-            fileName = "cars.dat";
+            fileName = "results.bin";
         }
-        
+
         ArrayList<Car> cars = loadFile(fileName);
-        
+
         acceptInput(cars);
     }
-    
+
     public static boolean checkFile(String[] args) {
         /* Checks if args[1] has a filename */
         if (args.length < 1) {
@@ -44,34 +47,33 @@ public class ArrayListObjectDemo {
         }
         return true;
     }
-    
+
     public static ArrayList<Car> loadFile(String fileName) {
         ArrayList<Car> loadCars = new ArrayList<>();
-        
+
         deserialize(loadCars, fileName);
-        
-        
-        
+
         return loadCars;
     }
-    
+
     public static void acceptInput(ArrayList<Car> cars) {
         boolean isUserFinished = false;
-        
+
         Scanner scan = new Scanner(System.in);
         String userInput = "";
-        
+
         while (!isUserFinished) {
             printMenu();
             try {
                 userInput = scan.next();
                 System.out.println("");
             } catch (InputMismatchException ex) {
-                
+
             }
-            
+
             switch (userInput) {
-                case "0": /* Exit program */
+                case "0":
+                    /* Exit program */
                     isUserFinished = true;
                     break;
                 case "1":
@@ -87,12 +89,20 @@ public class ArrayListObjectDemo {
                     break;
                 case "5":
                     break;
+                case "6":
+                    break;
+                case "7":
+                    deserialize(cars, "result.bin");
+                    break;
+                case "8":
+                    serialize(cars, "result.bin");
+                    break;
                 default:
                     break;
             }
         }
     }
-    
+
     public static void printMenu() {
         System.out.println("Array List Object Demo");
         System.out.println("======================");
@@ -101,34 +111,42 @@ public class ArrayListObjectDemo {
         System.out.println("2 - Display list of cars");
         System.out.println("3 - Sort list of cars");
         System.out.println("4 - Search for a car - UNIMPLEMENTED");
-        System.out.println("5 - Delete a car - UNIMPLEMENTED");
-        System.out.println("6 - Load list of cars - UNIMPLEMENTED");
-        System.out.println("7 - Save list of cars - UNIMPLEMENTED");
+        System.out.println("5 - Edit a car - UNIMPLEMENTED");
+        System.out.println("6 - Delete a car - UNIMPLEMENTED");
+        System.out.println("7 - Load list of cars");
+        System.out.println("8 - Save list of cars");
         System.out.println("");
         System.out.print("Enter a menu option here: ");
     }
-    
+
     public static void addNewCar(ArrayList<Car> cars, Scanner scan) {
         boolean isListFinished = false;
         try {
             while (!isListFinished) {
                 /* Get the make of the car from the user */
                 /* scan.nextLine() is used to ignore whitespaces */
+                String newMake;
+                String newModel;
+                String newYear;
+                
+                scan.useDelimiter("\n"); /* Use the ENTER key as a delimiter*/
+                int newOdometer = 0;
+                
                 System.out.print("Enter a car make: ");
-                scan.next();
-                String newMake = scan.nextLine();
+                //scan.hasNext();
+                newMake = scan.next();
 
                 /* Get the model */
                 System.out.print("Enter a car model: ");
-                String newModel = scan.nextLine();
+                newModel = scan.next();
 
                 /* Get the year */
                 System.out.print("Enter a car year: ");
-                String newYear = scan.nextLine();
+                newYear = scan.next();
 
                 /* Get the odometer */
                 System.out.print("Enter a car odometer: ");
-                int newOdometer = scan.nextInt();
+                newOdometer = scan.nextInt();
 
                 Car newCar = new Car(newMake, newModel, newYear, newOdometer);
 
@@ -141,71 +159,101 @@ public class ArrayListObjectDemo {
             System.out.println("Car was not added, odometer must be an integer value.");
         }
     }
-    
+
     public static boolean askForAnotherCar(Scanner scan) {
         System.out.print("Do you want to add a new car? [Y] or [N]: ");
-        
+
         Pattern p = Pattern.compile("[Y[y]]");
         Matcher m = p.matcher(scan.next());
         boolean b = m.matches();
-        
-        return !b;  /* The inverse of b must be returned to allow another car to be added */
+
+        return !b;
+        /* The inverse of b must be returned to allow another car to be added */
     }
-    
+
     public static void displayList(ArrayList<Car> cars) {
         /* This allows displayList() to be called independently with sortList() */
         displayList(cars, false);
     }
-    
+
     public static void displayList(ArrayList<Car> cars, boolean isListToBeSorted) {
         if (isListToBeSorted) {
             sortList(cars);
         }
-        
+
         /* Display a list of all cars */
         for (Car c : cars) {
             c.display();
         }
     }
-    
+
     public static void sortList(ArrayList<Car> cars) {
         Collections.sort(cars);
     }
-    
+
     public static void searchCar(ArrayList<Car> cars, String searchString) {
         int indx = cars.indexOf(searchString);
         //int result = Collections.binarySearch(cars, indx);
     }
-    
+
     public static void deleteCar(ArrayList<Car> cars, String deleteString, Scanner scan) {
         //try {
-        
+
         System.out.println("\r\nAre you sure you want to delete?");
         cars.remove(cars.indexOf(deleteString));
         //}
         //catch (Exception ex) {
-            
+
         //}
     }
-    
-    public static void serialize(String filename) {
-        
-    }
-    
-    public static void deserialize(ArrayList<Car> cars, String filename) {
-        Car object = null;
-        try {
-            FileInputStream file = new FileInputStream(filename);
-            ObjectInputStream in = new ObjectInputStream(file);
-            
-            object = (Car)in.readObject();
-            
-            in.close();
-            file.close();            
-        } catch (IOException ex) {
-            
-        } catch (ClassNotFoundException ex) {
-            
+
+    public static void serialize(ArrayList<Car> cars, String filename) {
+        Vector<Car> resultVector = new Vector<>();
+
+        for (Car c : cars) {
+            resultVector.add(c);
         }
+        System.out.println("Writing to file...");
+
+        try {
+            FileOutputStream fileOut = new FileOutputStream(filename); // binary file "result.bin"
+            ObjectOutputStream objectOut = new ObjectOutputStream(fileOut); // output stream
+
+            objectOut.writeObject(resultVector); // save the object in the binary file
+            fileOut.close(); // close the file
+            System.out.println("Completed - result.bin"); // user feedback
+        } catch (IOException e) { // catch if IO exceptions happen
+            System.out.println(e);
+        }
+    }
+
+    public static void deserialize(ArrayList<Car> cars, String filename) {
+        Vector v = new Vector();
+        try {
+            FileInputStream fileIn = new FileInputStream(filename);
+            ObjectInputStream objectIn = new ObjectInputStream(fileIn);
+
+            v = (Vector) objectIn.readObject();
+
+            objectIn.close();
+            fileIn.close();
+            
+            String pluralText = "";
+            if (v.size() > 1) {
+               pluralText = "s";
+            }
+            System.out.println(v.size() + " car" + pluralText + " imported");
+            
+            
+        } catch (IOException err) { // handle IO exception
+            System.err.println(err);
+        } catch (ClassNotFoundException err) { // handle ClassNotFound exception
+            System.err.println(err);
+        }
+
+//        for (int i = 0; i < v.size(); i++) {
+//            Car temp = (Car) v.elementAt(i);
+//            temp.display();
+//        }
     }
 }
